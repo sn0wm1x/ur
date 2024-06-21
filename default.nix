8d9c58f9f3fa179bfb9390bd0d45f718e2cb37ee
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-06-21 22:47:52 +0800

    chore: move pkgs to by-name

--- default.nix
+++ default.nix
@@ -17,4 +17,4 @@
   # example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
-} // builtins.mapAttrs (name: _: pkgs.callPackage (./pkgs + "/${name}") { }) (builtins.readDir ./pkgs)
+} // builtins.mapAttrs (name: _: pkgs.callPackage (./pkgs/by-name + "/${name}") { }) (builtins.readDir ./pkgs/by-name)
