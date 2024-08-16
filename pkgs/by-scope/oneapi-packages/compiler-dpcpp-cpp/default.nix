83021e2d8ab2d4d90fb4cd66ad7b169218f513e5
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-16 23:50:15 +0800

    oneapiPackages.compiler-dpcpp-cpp: init at  2024.2.1.1079

---
+++ pkgs/by-scope/oneapi-packages/compiler-dpcpp-cpp/default.nix
@@ -0,0 +1,50 @@
+{ lib
+, stdenvNoCC
+, fetchurl
+, rpmextract
+}:
+let
+  major = "2024.2";
+  minor = "1";
+  rel = "1079";
+in
+stdenvNoCC.mkDerivation ({
+  pname = "intel-oneapi-compiler-dpcpp-cpp";
+  version = "${major}.${minor}.${rel}";
+  preferLocalBuild = true;
+
+  src = fetchurl
+    {
+      url = "https://yum.repos.intel.com/oneapi/intel-oneapi-compiler-dpcpp-cpp-${major}-${major}.${minor}-${rel}.x86_64.rpm";
+      hash = "";
+    };
+
+  nativeBuildInputs = [ rpmextract ];
+
+  unpackPhase = "rpmextract $src";
+
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir $out/bin
+    mv opt/intel/oneapi/compiler/${major}/bin/ $out/bin/
+
+    mkdir $out/lib
+    mv opt/intel/oneapi/compiler/${major}/lib/ $out/lib/
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    broken = true; # WIP
+    description = "Intel oneAPI DPC++/C++ Compiler";
+    longDescription = " Standards driven high performance cross architecture DPC++/C++ compiler";
+    homepage = "https://software.intel.com/content/www/us/en/develop/tools/oneapi.html";
+    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+    license = licenses.issl;
+    platforms = [ "x86_64-linux" ];
+    maintainers = with maintainers; [ kwaa ];
+  };
+})
--- pkgs/by-scope/oneapi-packages/default.nix
+++ pkgs/by-scope/oneapi-packages/default.nix
@@ -1,5 +1,6 @@
 { lib, newScope, ... }:
 lib.makeScope newScope (self: with self; {
+  compiler-dpcpp-cpp = callPackage ./compiler-dpcpp-cpp { };
   llvm = callPackage ./llvm { };
   mpi = callPackage ./mpi { };
 })
